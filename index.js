1de3230850f002cd47ccaffe0452b1f936c1524b
Cliff Rodgers <kliph@users.noreply.github.com>
2018-09-14 12:22:33 -0400

    Render a small grey square using Processing.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("lol")
+const sketch = (processing) => {
+  processing.draw = () => {
+    processing.background(224);
+  }
+}
+
+const canvas = document.getElementById('canvas');
+const processingInst = new Processing(canvas, sketch);
